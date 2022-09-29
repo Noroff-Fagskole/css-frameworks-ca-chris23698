56c72750dd23b5e81f2a45a96b03e015ca7d9a5d
Christoffer Brendemo <chris23698@gmail.com>
2022-09-29 14:37:51 +0200

    changed color to pink

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
       colors: {
         "primary": "#0F1417",
         "secondary": "#141A1F",
-        "main-text": "#A9B9E6",
+        "main-text": "#A4036F",
         "input-color": "#202934",
         "gmail": "#FF3B3B",
         "facebook": "#16148E",
         "linkedin": "#3DA2FF",
-        "hover-btn": "#6B8DEE"
+        "hover-btn": "#4E0034"
       }
     },
   },
